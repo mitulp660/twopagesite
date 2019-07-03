fbf92c5df67523b2879a629b7186ff47c418c6d6
mitul <mitulp660@gmail.com>
2019-07-03 15:23:21 -0400

    testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mitul Patel PaGe
                 </div>
 
                 <div class="links">
