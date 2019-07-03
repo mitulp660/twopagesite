20d3b6fb69066a41f23060cff489b3f057e0e7e9
mitul <mitulp660@gmail.com>
2019-07-03 18:32:57 -0400

    editing for about page

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -29,13 +29,10 @@
                         <a class="nav-link" href="/login">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/get-started">Get Started</a>
+                        <a class="nav-link" href="/about">Get Started</a>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                    <button class="btn btn btn-success my-2 my-sm-0" type="submit">Find Courses</button>
-                </form>
+
             </div>
         </div>
     </nav>
