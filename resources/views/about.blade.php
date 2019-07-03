70b94fc2ea95a520c1cacd141ac6f2e862c15e97
mitul <mitulp660@gmail.com>
2019-07-03 15:33:05 -0400

    making changes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Mitul Patel PaGe
+                    ABOUT
                 </div>
 
                 <div class="links">
