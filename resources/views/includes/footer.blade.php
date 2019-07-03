a2efcf9055ca95b81cd2ce42f7da9b5e900eb9e7
mitul <mitulp660@gmail.com>
2019-07-03 19:10:21 -0400

    error fixing

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,3 @@
-<div class="row mt-4 mb-4"  class="font-weight-light">
+<div class="font-weight-light">
     &copy; 2019-Summer,class 601.&middot;  <a href="#"  class="font-weight-light">Mitul</a> &middot; <a href="#"  class="font-weight-light">Patel</a>
 </div>
