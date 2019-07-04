d1e4ba4e5f029fa15d36f93be0dc4b017e38e4ac
mitul <mitulp660@gmail.com>
2019-07-04 13:21:54 -0400

    making change to make the site work properly

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Two-Page_HW</title>
-        <link rel="stylesheet" href="/css/layouts.css">
+        <link rel="stylesheet" href="/css/app.css">
 
     </head>
     <body>
