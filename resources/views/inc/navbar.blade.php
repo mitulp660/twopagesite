c876ed152bce660bb5cda44935d4eeb8eccb5fb5
mitul <mitulp660@gmail.com>
2019-07-04 15:26:32 -0400

    fixing the error by adding new navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -6,11 +6,11 @@
 
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            <li>
+                <a  href="/">Home <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/about">about</a>
+            <li>
+                <a href="/about">about</a>
             </li>
 
         </ul>
--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -8,18 +8,8 @@
     </head>
     <body>
         @include('inc.navbar')
-
-
-
-            <div class="row">
-                <div class="col-md-8 col-lg-8">
-                    @yield('content')
-                </div>
-
-                <div class="col-md-4 col-lg-4">
-                    @include('inc.sidebar')
-                 </div>
-            </div>
+        @yield('content')
+        @include('inc.sidebar')
 
 
     </body>
